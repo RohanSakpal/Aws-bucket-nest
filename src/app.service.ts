15eb980d810a7d8ebeaaf8ce13ef2b58e2348214
Rohan Sakpal <rohan.sakpal@atriina.com>
2024-09-11 11:34:13 +0530

    First Commit

---
+++ src/app.service.ts
@@ -0,0 +1,50 @@
+import { Injectable, Req, Res } from '@nestjs/common';
+import * as AWS from 'aws-sdk';
+
+@Injectable()
+export class AppService {
+  AWS_S3_BUCKET = 'demo-nest';  //bucket name
+  s3 = new AWS.S3({
+    accessKeyId: 'AKIAVD23BXxxxxxxxxxx',
+    secretAccessKey: 'uwVxyznsJ7PAfgQv4dZBQ5TuZxxxxxxxxxxxxxxx',
+  });
+
+
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  async uploadFile(file) {
+    console.log(file);
+    const { originalname } = file;
+
+    return await this.s3_upload(
+      file.buffer,
+      this.AWS_S3_BUCKET,
+      originalname,
+      file.mimetype,
+    );
+  }
+
+  async s3_upload(file, bucket, name, mimetype) {
+    const params = {
+      Bucket: bucket,
+      Key: String(name),
+      Body: file,
+      ACL: 'public-read',
+      ContentType: mimetype,
+      ContentDisposition: 'inline',
+      CreateBucketConfiguration: {
+        LocationConstraint: 'ap-south-1',
+      },
+    };
+
+    try {
+      let s3Response = await this.s3.upload(params).promise();
+      return s3Response;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+}
